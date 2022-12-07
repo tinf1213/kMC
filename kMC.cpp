52b39f1ca7de49d97b7bbe585f4e32a8d234f083
tinf1213 <ericding1213@gmail.com>
2022-12-07 16:21:17 +0800

    fix ktot

--- kMC.cpp
+++ kMC.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 double concentrate_ZnO, concentrate_O3_aq = 55.2 / pow(10, 6), concentrate_H2O2 = 0, concentrate_O2 = 0, concentrate_Odot = 0, concentrate_O3_ad = 0, concentrate_H2O = 55.5;
-double ktot, delta_time;
+double delta_time;
 long long MC_counter = 0;
+const double ktot = 9300 + 740 + 3150 + 14400 + 0.075 + 420000000;
+double time_accumulated = 0;
 vector<double> react_const = {9300, 740, 3150, 14400, 0.075, 420000000};
+vector<double> Cumulated_const(6,0);
 
 /*
 define react
@@ -24,26 +27,25 @@ void calculate_Rates(vector<double>& Rates){
     Rates[5] = react_const[5] * concentrate_H2O2 * concentrate_ZnO;
     //for(int i=0;i<6;i++) cout << Rates[i] << endl;
 }
-void calculate_Cumulated_Rates(vector<double>& Rates, vector<double>& Cumulated_Rates){
-    Cumulated_Rates[0] = Rates[0];
+void calculate_Cumulated_const(){
+    Cumulated_const[0] = react_const[0];
     for(int i=1;i<6;i++){
-        Cumulated_Rates[i] = Cumulated_Rates[i-1] + Rates[i];
+        Cumulated_const[i] = Cumulated_const[i-1] + react_const[i];
     }
 }
-int select_event(vector<double>& Cumulated_Rates){
-    ktot = Cumulated_Rates[5];
-    cout << ktot << endl;
+
+int select_event(vector<double>& Cumulated_const){
     double u1 = (double)rand()/RAND_MAX;
     double target = u1 * ktot;
-    if(Cumulated_Rates[0] > target) return 0;
+    if(Cumulated_const[0] > target) return 0;
     for(int i=1;i<6;i++){
-        if(Cumulated_Rates[i-1]<target && Cumulated_Rates[i] >= target) return i;
+        if(Cumulated_const[i-1]<target && Cumulated_const[i] >= target) return i;
     }
 }
 void update_time(){
     double u2 = (double)rand()/RAND_MAX;
     //cout << ktot << endl;
-    delta_time = log(1/u2)/ktot;
+    delta_time = -1*log(u2)/ktot;
     //cout << delta_time << endl;
 }
 void update_concentrate(int &event, vector<double> &Rates){
@@ -77,35 +79,35 @@ void update_concentrate(int &event, vector<double> &Rates){
 }
 void init_dat(double& ZnO_weigth){
     cout << "#MC-step   times(s)       [O3](um)" << endl;
-    cout << 0 << "          " << "0.000" << "          " << ZnO_weigth << endl;
+    cout << 0 << "          " << "0.000" << "          " << 55.2 << endl;
 }
 int main(){
     srand(time(NULL));                                                             
-    double ZnO_weigth = 55.2; //cin >> ZnO_weigth;  
+    double ZnO_weigth = 10; //cin >> ZnO_weigth;  
     init_dat(ZnO_weigth);               
     concentrate_ZnO = ZnO_weigth / 81.38 * pow(10, 3);
     int MC_counter_accumulated = 0;
+    calculate_Cumulated_const();
     while(1){
-        double time_accumulated = 0;
         vector<double> Rates(6, 0);
         vector<double> Cumulated_Rates(6, 0);
         calculate_Rates(Rates);
-        calculate_Cumulated_Rates(Rates, Cumulated_Rates);
-        int event = select_event(Cumulated_Rates);
+        int event = select_event(Cumulated_const);
         //cout << event << endl;
         update_time();
         time_accumulated += delta_time;
-        //cout << delta_time << endl;
         update_concentrate(event, Rates);
+        //cout << time_accumulated << endl;
+        //cout << delta_time << endl;
         MC_counter++;
         if(MC_counter == 100){
             MC_counter_accumulated += MC_counter;
             cout << MC_counter_accumulated << "        " << time_accumulated << "    " << (concentrate_O3_aq) * pow(10, 6) << endl;
             MC_counter = 0;
         }
-        if(time_accumulated >= 1800) break;
-        //if(MC_counter >= 100) break;
-        if(MC_counter_accumulated >= 2) break;
+        //if(time_accumulated >= 1800) break;
+        //if(MC_counter >= 10) break;
+        if(MC_counter_accumulated >= 2000) break;
         // break;
     }
     return 0;
